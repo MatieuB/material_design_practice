0a7f4373b26058897232cce7aeb603a1e4af3373
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-18 08:15:01 -0600

    initial commit

---
+++ public/javascripts/module.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+
+  angular.module('matthew',['ngAnimate','ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl:"./components/home/home.html",
+      controller: function($scope){
+        $scope.hidden1 = true;
+        $scope.hidden2 = true;
+        $scope.hidden3 = true;
+        $scope.show1 = function(){
+          $scope.hidden1 = !$scope.hidden1;
+        }
+        $scope.show2 = function(){
+          $scope.hidden2 = !$scope.hidden2;
+        }
+        $scope.show3 = function(){
+          $scope.hidden3 = !$scope.hidden3;
+        }
+      }
+    });
+    // $stateProvider.html5Mode(true)
+  });
+  // angular.module('Reddit').factory('authInterceptor', function () {
+  //   return {
+  //     request: function(config) {
+  //       if (localStorage.getItem('token')) {
+  //         config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  //       }
+  //       return config;
+  //     },
+  //     responseError: function(response) {
+  //       console.log('from the interceptor: ',response);
+  //       return response;
+  //     }
+  //   };
+  // })
+}());
