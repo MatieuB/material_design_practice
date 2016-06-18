42a036d5d6b8c1964d5de81eddc571c5fd7da1c9
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-18 15:03:48 -0600

    decent layout taking shape

--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -17,12 +17,23 @@
         $scope.hidden3 = true;
         $scope.show1 = function(){
           $scope.hidden1 = !$scope.hidden1;
+          $scope.hidden2 = true;
+          $scope.hidden3 = true;
         }
         $scope.show2 = function(){
           $scope.hidden2 = !$scope.hidden2;
+          $scope.hidden1 = true;
+          $scope.hidden3 = true;
         }
         $scope.show3 = function(){
           $scope.hidden3 = !$scope.hidden3;
+          $scope.hidden2 = true;
+          $scope.hidden1 = true;
+        }
+        $scope.hideAll = function(){
+          $scope.hidden1 = true;
+          $scope.hidden2 = true;
+          $scope.hidden3 = true;
         }
       }
     });
