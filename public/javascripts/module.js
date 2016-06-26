127d7304b4e15a0666dcd5dbe4ca29c775dbd575
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-06-25 21:08:00 -0600

    deleted a lot of cruft

--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -8,77 +8,19 @@
     .state('home', {
       url: '/',
       templateUrl:"./components/home/home.html"
-      // controller: function($scope){
-      //   $scope.hidden1 = true;
-      //   $scope.hidden2 = true;
-      //   $scope.hidden3 = true;
-      //   $scope.show1 = function(){
-      //     $scope.hidden1 = !$scope.hidden1;
-      //     $scope.hidden2 = true;
-      //     $scope.hidden3 = true;
-      //   }
-      //   $scope.show2 = function(){
-      //     $scope.hidden2 = !$scope.hidden2;
-      //     $scope.hidden1 = true;
-      //     $scope.hidden3 = true;
-      //   }
-      //   $scope.show3 = function(){
-      //     $scope.hidden3 = !$scope.hidden3;
-      //     $scope.hidden2 = true;
-      //     $scope.hidden1 = true;
-      //   }
-      //   // $scope.hideThings = function(){
-      //   //   console.log("clicked");
-      //   //   if($scope.hidden1 == false ){
-      //   //     $scope.hidden1 = true;
-      //   //   }
-      //   //   if($scope.hidden2 === false ){
-      //   //     $scope.hidden2 = true;
-      //   //   }
-      //   //   if($scope.hidden3 === false ){
-      //   //     $scope.hidden3 = true;
-      //   //   }
-      //   // }
     })
     .state('projects', {
       url: '/projects',
       templateUrl:"../components/home/projects.html",
       controller: function($scope){
-
       }
-
-    })
-    .state('about', {
-      url: '/about',
-      templateUrl:"../components/home/about.html",
-      controller: function($scope){
-
-      }
-
     })
     .state('resume', {
       url: '/resume',
       templateUrl:"./components/home/resume.html",
       controller: function($scope){
-
       }
-
     })
     // $stateProvider.html5Mode(true)
   })
-
-  // angular.module('Reddit').factory('authInterceptor', function () {
-  //   return {
-  //     request: function(config) {
-  //       if (localStorage.getItem('token')) {
-  //         config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
-  //       }
-  //       return config;
-  //     },
-  //     responseError: function(response) {
-  //       console.log('from the interceptor: ',response);
-  //       return response;
-  //     }
-  //   };
-  // })
 }());
